73d069c328b20eec63a381edcec4218d018fda62
Ankit Mitra <ankitmitra486@gmail.com>
2020-12-14 12:10:25 +0530

    A fun automation code

---
+++ Python_Projects/automation.py
@@ -0,0 +1,9 @@
+#pip install pyautogui
+import pyautogui
+import time
+
+i = 0
+while i < 10:
+    pyautogui.write('# Everything fine friends?', interval= 0.1)
+    pyautogui.press('enter')
+    i += 1
